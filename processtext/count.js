ce7d7261d0c9042bf0ed4d4c8e7648f851a29e95
ErikaHamilton <hamiltone@g.cofc.edu>
2018-10-20 21:53:57 -0400

    undefined issues :(

--- processtext/count.js
+++ processtext/count.js
@@ -4,29 +4,46 @@
 
 console.clear();
 function Parse() {
-var inputArr = document.getElementById("input").innerHTML;
-var wordArr = inputArr.toUpperCase(); //Changes user input into upper case
-wordArr = wordArr.replace(/[!"#$%&'()*+,-./:;<=>?@[^_`{|}~]/,"");
-var totalCount;
-var wordString;
-//loops through each element to copy
-for (i = 0; i < wordArr.length; i++) {
-  var comp = wordArr[i]; //creates a copy to compare
-  var matched = false; //Makes them all unknown
-  //loops through each element again and compares elements
-  for(j = 0; j < wordString.length; j++) {
-    if (comp = wordString[i]) {
+//------------------------------------ Declaring Variables
+  var totalCount;
+  var wordString;
+  var comp;
+  var i;
+  var j;
+  var k;
+  var matched;
+  var output = new Array();
+  var wordArr = new Array();
+  var inputArr = new Array();
+
+//------------------------------------ Organizing Variables
+  inputArr = document.getElementById("input").value;
+  console.log(inputArr);
+ //Changes user input into upper case
+  wordArr = inputArr.toUpperCase();
+  console.log(wordArr);
+  wordArr = wordArr.split( ",");
+  console.log(wordArr);
+  console.log(wordArr.length);
+  var k = wordArr.length;
+  //loops through each element to copy
+  for (i = 0; i < k; i++) {
+    var comp = wordArr[i]; //creates a copy to compare
+    var matched = false; //Makes them all unknown
+    //loops through each element again and compares elements
+
+    for(j = 0; j < wordString.length; j++) {
+    if (comp == wordString[i]) {
       totalCount = totalCount[i] + 1
       matched = true;
       break;
     }
   }
-  if (matched = false) {
+  if (matched == false) {
     //creates final arrays of information
     wordString.push(comp);
     totalCount.push(1);
   }
-  var output = new Array();
   output = output + wordString[i] + "" + totalCount[i];
   console.log(output);
   console.log(totalCount);
