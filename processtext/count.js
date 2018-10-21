866fe15abca74577816e5026ce50877c4bacc4c3
ErikaHamilton <hamiltone@g.cofc.edu>
2018-10-20 21:09:23 -0400

    Most recent update

--- processtext/count.js
+++ processtext/count.js
@@ -3,7 +3,34 @@
 // Word Count JS
 
 console.clear();
+function Parse() {
+var inputArr = document.getElementById("input").innerHTML;
+var wordArr = inputArr.toUpperCase(); //Changes user input into upper case
+wordArr = wordArr.replace(/[!"#$%&'()*+,-./:;<=>?@[^_`{|}~]/,"");
+var totalCount;
+var wordString;
+//loops through each element to copy
+for (i = 0; i < wordArr.length; i++) {
+  var comp = wordArr[i]; //creates a copy to compare
+  var matched = false; //Makes them all unknown
+  //loops through each element again and compares elements
+  for(j = 0; j < wordString.length; j++) {
+    if (comp = wordString[i]) {
+      totalCount = totalCount[i] + 1
+      matched = true;
+      break;
+    }
+  }
+  if (matched = false) {
+    //creates final arrays of information
+    wordString.push(comp);
+    totalCount.push(1);
+  }
+  var output = new Array();
+  output = output + wordString[i] + "" + totalCount[i];
+  console.log(output);
+  console.log(totalCount);
+}
 
-var divNode = document.getElementById("count");
-// prompt
-// parse and Count
+document.getElementById("output").innerHTML = output;
+}
