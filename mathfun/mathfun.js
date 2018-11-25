f0938d7c71b1c1b19ed0c78bf5d1474ac571f832
ErikaHamilton <hamiltone@g.cofc.edu>
2018-11-25 16:57:47 -0500

    I have no idea why this mathfun program is so difficult

--- mathfun/mathfun.js
+++ mathfun/mathfun.js
@@ -3,176 +3,155 @@
 // Assignment 2
 
 console.clear();
-function Prompt () {
-//------------------------------------ Declaring Variables
-  var mean;
-  var sum;
-  var med;
-  var max;
-  var min;
-  var evens;
-  var odds;
-  var primes;
-  var power2;
-  var num = "";
   var numarr = new Array();
-
-//----------------------------------------------- Beginning of Prompt
-//do while?
+//----------------------------------------------- Beginning of Prompt Function
+function Prompt () {
+  do {
+    var num = prompt("Enter a set of positive numbers, Enter -1 when you are finished.");
+    numarr.push(num);}
   while (num != '-1') {
-    num = +prompt("Enter a set of positive numbers, Enter -1 when you are finished.");
-    numarr.push(Number(num)); // Pushes into the array as ints
   }
-//Getting the Array into order and excluding the -1.
-  numarr.pop(); //Must remove last element since it was a -1.
+  //Getting the Array into order and excluding the -1.
+  numarr.pop();
   numarr.sort(function(a,b) { return a-b});  //Set the list of numbers into ascending order, was stolen from w3schools, but makes sense.
 
-  //saftey
-  console.log(num); //Prints the sequence entered
+//----------------------------------------------- FLAGS
   console.log(numarr); //prints sorted array
-
-  //Delete this all eventually, half of it is useless,
+//----------------------------------------------- Function Calls
+  document.getElementById("seq").innerHTML = "[" + numarr + "]";
   var max = Max(numarr);
   var min = Min(numarr);
   var med = Median(numarr);
   var mean = Mean(numarr);
-  var odds = Odd(numarr);
+  // var odds = Odd(numarr);
   var evens = Even(numarr);
   var primes = Primes(numarr);
   var power2 = PowerTwo(numarr);
-  console.log("The max is:", max);
-  console.log("The min is:", min);
-  console.log("The med is:", med);
-  console.log("The mean is:", mean);
-  console.log("The odd count is:", odds);
-  console.log("The even count is:", evens);
-  console.log("The primes count is:", primes);
-  console.log("The powers count is:", power2);
+
+}
 
 //---------------------------------------------------- Mean Function
 function Mean(numarr) {
-  sum = 0;
+  var sum = 0;
   for (var i = 0; i < numarr.length; i++) {
     sum = sum + numarr[i];
   }
-  mean = 0;
-  mean = sum/numarr.length;
-  return Math.floor(mean);
+  console.log("total sum: " + sum);
+  var mean = 0;
+  mean = sum / numarr.length;
+
+  mean = Math.floor(mean);
+  document.getElementById("mean").innerHTML = mean;
+  return mean;
 }
 
 //------------------------------------------------  Median Function
 
 function Median(numarr) {
-  var medIndex = 0;
-  if ((numarr.length % 2) !=0) //Mod division will split the sorted array in half
-  {
-    medIndex = 0;
-    med = 0;
-    medIndex = Math.floor(numarr.length / 2);//This next function grabs the index of the closest
-    med = numarr[medIndex];
-  }
-  else if(numarr.length % 2 == 0)
-  {
-    med = (numarr[i] + numarr[index-1]) / 2;//Returns an average if the num.length is even.
-  }
-  return Math.floor(med);
+    var medIndex = Math.floor(numarr.length / 2);//This next function grabs the index of the closest
+    var med = numarr[medIndex];
+    med = med[medIndex];
+    document.getElementById("med").innerHTML = med;
+ return med;
 }
 
 //------------------------------------------------ Maximum Function
 
-function Max(numaar) {
-  max = numarr[numarr.length-1];
+function Max(numarr) {
+  var max = numarr[numarr.length-1];
+  document.getElementById("max").innerHTML = max;
   return max;
 }
 
 //------------------------------------------------ Minimum Function
 
 function Min(numarr) {
-  min = numarr[0];
-
+  var min = numarr[0];
+  document.getElementById("min").innerHTML = min;
   return min;
 }
 
 //------------------------------------------------ Even Function
 
 function Even(numarr) {
-  for( i = 0; i < numarr.length; i++) {
-    // check = numarr[i] % 2;
-
-    if((numarr[i] % 2) == 0) {
-
-    evens += 1;
+  var elementCt = numarr.length;
+  console.log("Array Length:" +elementCt);
+  for(var i = 0; i < elementCt; i++) {
+    var check = numarr[i] % 2;
+    if(check == 0) {
+      var evenCt = evenCt + 1;
+      console.log("Evens:" + evenCt);
 
     }
 
-    else { //it's odd
+    else {
+
   }
 }
-  return evens;
+  document.getElementById("evens").innerHTML = evenCt;
+
+  return evenCt;
 }
-//------------------------------------------------ Odd Function
+// //------------------------------------------------ Odd Function
 
 function Odd(numarr) {
-  for( i = 1; i < numarr.length; i++) {
-    check = numarr[i] % 2;
-
+  var elementCt = numarr.length;
+  for(var i = 1; i < elementCt; i++) {
+    var check = numarr[i] % 2;
     if(check != 0) {
-    odds += 1;
+    var  oddsCt = oddsCt + 1;
     }
+    console.log("Odds: " + oddsCt);
 
-    else {}
+    else {
+
+    }
 }
 
-  return odds;
+  document.getElementById("odds").innerHTML = oddCt;
+  return oddCt;
 }
 
 //------------------------------------------------ Powers of 2 Function
 
 function PowerTwo(numarr) {
-  for( i = 0; i < numarr.length; i++) {
-    x = numarr[i];
-    rootval = Math.pow(x, 1/x);
+  var elementCt = numarr.length;
+  for(var i = 0; i < elementCt; i++) {
+    var x = numarr[i];
+    var rootval = Math.pow(x, 1/x);
 
     if(rootval == 2) {
-      power2 =+ 1;
+      var power2Ct = power2Ct + 1;
     }
 
     else {
     }
 }
 
-  return power2;
+  document.getElementById("power2").innerHTML= power2Ct;
+
+  return power2Ct;
 }
 //------------------------------------------------ Primes Function
 
 function Primes(numarr) {
-  for( i = 0; i < numarr.length; i++) {
-    x = numarr[i];
+  var elementCt = numarr.length;
+  for(var i = 0; i < elementCt; i++) {
+    var x = numarr[i];
     // div2 = x % 2;
-    fsquare = Math.floor(Math.sqrt(numarr));
+    var fsquare = Math.floor(Math.sqrt(x));
     //Prime iff it is not div. by any prime <= its square root
     for(x > 2; i <= fsquare; i++) {
       if(x % i == 0){
       //It's not prime, so we will count when it is
     }
       else {
-        primes =+ 1;
+        var primeCt =+ 1;
         //it is prime
       }
     }
   }
-return primes;
-}
+ document.getElementById("primes").innerHTML = primeCt;
 
-//------------------------------------------------ Putting it all into html
-document.getElementById("seq").innerHTML = "[" + numarr + "]";
-document.getElementById("max").innerHTML = Max(numarr);
-document.getElementById("min").innerHTML = Min(numarr);
-document.getElementById("med").innerHTML = Median(numarr);
-document.getElementById("mean").innerHTML = Mean(numarr);
-document.getElementById("odds").innerHTML = Odd(numarr);
-document.getElementById("evens").innerHTML = Even(numarr);
-document.getElementById("primes").innerHTML = Primes(numarr);
-document.getElementById("power2").innerHTML = PowerTwo(numarr);
-
+ return primeCt;
 }
